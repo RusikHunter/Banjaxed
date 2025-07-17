5e81e32d967fdf7aa31de459bcd3b93e9675e2e4
dmfshove <funnymoments610@gmail.com>
2025-07-17 23:11:52 +0200

    chore(app): build v1.1

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import HeaderHandler from './scripts/HeaderHandler.js'
+import TabClickHandler from './scripts/TabClickHandler.js'
+import ConversationsAnimationHandler from './scripts/ConversationsAnimationHandler.js'
+import ScrollDecorationHandler from './scripts/ScrollDecorationHandler.js'
+import SliderReviewHandler from './scripts/SliderReviewHandler.js'
+import FormHandler from './scripts/FormHandler.js'
+import './styles/styles.scss'
+
+new HeaderHandler()
+new TabClickHandler()
+new ConversationsAnimationHandler()
+new ScrollDecorationHandler()
+new SliderReviewHandler()
+new FormHandler()
